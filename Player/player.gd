5806666fb4516d866fe602d66efd22fb99d0850e
Justin Villerot - HyprPC <62113284+VillerotJustin@users.noreply.github.com>
2025-10-27 16:22:48 +0100

    Move World Scene & Player Movement + shoot bases

--- Player/player.gd
+++ Player/player.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED: float = 300.0
 
+func get_movement_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+	
+func shooting():
+	var is_shooting: bool = false
+	var shooting_direction: Vector2 = Vector2.ZERO
+	
+	if Input.is_action_just_pressed("shoot_left") and ! (Input.is_action_just_pressed("shoot_right") or Input.is_action_just_pressed("shoot_up") or Input.is_action_just_pressed("shoot_down")):
+		is_shooting = true
+		shooting_direction.x -= 1
+	
+	if Input.is_action_just_pressed("shoot_right") and ! (Input.is_action_just_pressed("shoot_left") or Input.is_action_just_pressed("shoot_up") or Input.is_action_just_pressed("shoot_down")):
+		is_shooting = true
+		shooting_direction.x += 1
+	
+	if Input.is_action_just_pressed("shoot_up") and ! (Input.is_action_just_pressed("shoot_left") or Input.is_action_just_pressed("shoot_right") or Input.is_action_just_pressed("shoot_down")):
+		is_shooting = true
+		shooting_direction.y -= 1
+	
+	if Input.is_action_just_pressed("shoot_down") and ! (Input.is_action_just_pressed("shoot_left") or Input.is_action_just_pressed("shoot_right") or Input.is_action_just_pressed("shoot_up")):
+		is_shooting = true
+		shooting_direction.y += 1
+		
+	if is_shooting and shooting_direction != Vector2.ZERO:
+		shooting_direction = shooting_direction.normalized()
+		
+	
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(_delta: float) -> void:
+	# Moving the Character
+	get_movement_input()
 	move_and_slide()
